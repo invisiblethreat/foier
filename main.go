3d9f37d9e8c795661cab940601985ee5a25de095
Scott Walsh <swalsh@securityscorecard.io>
2018-04-26 10:28:33 -0300

    doing what needs to be done

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/pflag"
 )
 
-// CLIOptions are to set things via CLI
+// CLIOptions are to set things via CLI.
 type CLIOptions struct {
 	Debug   bool
 	URI     string
@@ -19,33 +19,50 @@ type CLIOptions struct {
 
 func main() {
 
+	// Declare the variable before we use it.
 	var cli CLIOptions
+
+	// Establish where we should hold the values from our CLI options.
 	pflag.BoolVarP(&cli.Debug, "debug", "d", false, "Use debug mode")
-	pflag.StringVarP(&cli.URI, "uri", "u",
-		"https://foipop.novascotia.ca/foia/views/_AttachmentDownload.jsp?attachmentRSN=",
-		"URI to use for fetching")
+	pflag.StringVarP(&cli.URI, "uri", "u", "", "URI to use for fetching")
 	pflag.IntVarP(&cli.Start, "start", "s", 0, "Starting ID")
 	pflag.IntVarP(&cli.End, "end", "e", 7000, "End ID")
 	pflag.IntVarP(&cli.Step, "increment", "i", 1, "Increment between steps")
-	pflag.StringVar(&cli.Suffix, "suffix", "", "Suffix of the file you're downloading")
+	pflag.StringVar(&cli.Suffix, "suffix", "x", "Suffix of the target files")
 	pflag.IntVarP(&cli.Workers, "workers", "w", 5, "How many workers to use")
 
 	pflag.Parse()
 
+	// Are we going to use debug mode?
 	if cli.Debug {
 		logrus.SetLevel(logrus.DebugLevel)
 	}
 
-	logrus.Debugf("Logging level: %s", logrus.GetLevel().String())
+	// Show the current log level- the default is logrus.InfoLevel.
+	logrus.Infof("Logging level: %s", logrus.GetLevel().String())
 
-	config := foier.Config{
-		URI:     cli.URI,
-		Start:   cli.Start,
-		End:     cli.End,
-		Step:    cli.Step,
-		Workers: cli.Workers,
-		Suffix:  cli.Suffix,
-	}
+	// Convert the CLI arguments into a configuration that Foier can understand.
+	config := cli.mapToFoier()
 
+	// We're all done with setup and now we're going to do the real work. All of
+	// the items here could have been hardcoded inside the 'Scrape' method if
+	// goal was the fewest lines of code possible.
 	foier.Scrape(config)
+}
+
+// mapToFoier transforms takes one type and returns another. This type of action
+// is called a 'pointer receiver'. Since you're not attempting to change the
+// values in the initial structure, you do not need to pass by reference.
+// Self-modification of a struct requires that you pass a reference or your
+// changes will not persist upon return.
+func (c CLIOptions) mapToFoier() foier.Config {
+	config := foier.Config{
+		URI:     c.URI,
+		Start:   c.Start,
+		End:     c.End,
+		Step:    c.Step,
+		Workers: c.Workers,
+		Suffix:  c.Suffix,
+	}
+	return config
 }
--- scrape/scrape.go
+++ scrape/scrape.go
@@ -1,21 +1,31 @@
 package foier
 
+// The package name does not need to match the directory that it is placed in.
+// You can split a package across an arbitrary number of files as long as they
+// are in the same directory. 'package' must also be on the first line of the
+// file.
+
 import (
 	"io"
 	"net/http"
 	"os"
 	"strconv"
 	"sync"
-	"time"
 
+	// This import looks like it is remote, but it exists as a path within your
+	// $GOHOME, which defaults to ~/go if not set.
 	"github.com/sirupsen/logrus"
 )
 
 // type can be structered the same as import, or they can be standalone
-// statments- this format is editor friendly for code folding.
+// statments- this format is editor-friendly for code folding.
 type (
-	// Config sets the items needed for enumeration
+	// Config sets the items needed for enumeration. Since 'Config' is
+	// capitalized, it can be accessed as a type outside of this package with
+	// by importing the package and referenceing 'foier.Config'
 	Config struct {
+		// The member attributes are also public and can be set external to the
+		// package
 		URI     string
 		Suffix  string
 		Start   int
@@ -24,16 +34,15 @@ type (
 		Workers int
 	}
 
-	// Target is a struct for passing information
-	Target struct {
-		URI  string
-		Name string
-	}
-
-	// Document is a response to a request that will be saved
-	Document struct {
-		Name string
-		Body io.Closer
+	// target is a struct for passing information about what you want to fetch
+	// and save. It is lowercase and can not be accessed outside of this
+	// package. Other files within the package can access it without issue.
+	target struct {
+		// The member attribues are lowercase which makes them private to the
+		// package. This gives you the ability to change the internal workings
+		// of the package/structure, and not worry about external impacts.
+		uri  string
+		name string
 	}
 )
 
@@ -41,22 +50,34 @@ type (
 func Scrape(config Config) {
 
 	// initialize our channels
-	targets := make(chan (Target))
-	docs := make(chan (Document))
+	targets := make(chan (target))
 
 	// Generate all the targets in a goroutine. Pass the config through since we
-	// reuse so many values.
+	// reuse so many values. We use a goroutine so we don't block the rest of
+	// execution.
 	go genTargets(targets, config)
 
-	// WaitGroup is key for not orphaning your results.
+	// WaitGroup is key for not orphaning your results. In this case it is used
+	// as a semaphore to track the state of goroutines that are stil running.
+	// When the 'Wait()' method is called, the semaphore will block until the
+	// count reaches zero, meaning that all goroutines have completed, which
+	// prevents you from orphaning your results.
 	var wg sync.WaitGroup
 
 	// Spawn our workers
 	for i := 0; i < config.Workers; i++ {
+
+		// Add '1' to the WaitGroup, which is a blocking condition for the
+		// 'Wait()' method.
 		wg.Add(1)
+
 		// Workers live in the background, but with the WaitGroup we know when
-		// they finish all of their work.
-		go Worker(targets, docs, &wg)
+		// they finish all of their work. Passing 'wg' by reference, or the call
+		// to 'Done()' would decrement a copy and we'd be stuck waiting forever.
+		// Not using a WaitGroup would spawn off all of your workers to begin
+		// execution, but not block and the function would retun almost
+		// instantly and most of your results would be orphaned.
+		go Worker(targets, &wg)
 	}
 
 	// This call blocks further execution while the Workers are active. Without
@@ -65,57 +86,89 @@ func Scrape(config Config) {
 	// WaitGroup must become zero before the call stops blocking.
 	logrus.Debug("Waiting on the Workers to finish their jobs")
 	wg.Wait()
-
-	// Since all of the Workers have terminated we can now close the Document
-	// channel, which will allow the range to terminate.
-	logrus.Debug("Closing doc channel of type Document")
-	close(docs)
-
-	// There's technically a small race condition here, from when the last
-	// Worker closes and when the result is processed by the Saver in the
-	// background.
-
-	time.Sleep(1 * time.Second)
 }
 
-// Worker does all of the scraping
-func Worker(targets <-chan Target, docs chan<- Document, wg *sync.WaitGroup) {
+// Worker does all of the downloading. Notice that the WaitGroup is passed by
+// reference so all workers are reporting back to the same semaphore.
+func Worker(targets <-chan target, wg *sync.WaitGroup) {
+
+	// 'defer' is called after the function returns. 'Done()' decrements the
+	// semaphore by one, getting closer to the exit condition for 'Wait()'
 	defer wg.Done()
 
-	// Range over the channel until it is empty. This is blocking until the
-	// channel is closed.
+	// Range over the channel until it is empty. This call is blocking until the
+	// channel is closed. That is, a channel must both be empty and closed
+	// before this loop will terminate.
 	for target := range targets {
-		res, err := http.Get(target.URI)
+
+		// Get the target document.
+		res, err := http.Get(target.uri)
+
+		// This is a code tick that you'll see repeatedly in Golang. Errors are
+		// just values and you should handle them accordingly.
 		if err != nil {
+			// This is a silent failure
 			logrus.Debugf("Error fetching %s: %s", target, err.Error())
 			continue
 		}
 
-		out, err := os.Create("data/" + target.Name)
+		// Make the file to contain the results of the document
+		out, err := os.Create("data/" + target.name)
+		// The tick again
 		if err != nil {
-			logrus.Errorf("Error creating file %s: %s", target.Name, err.Error())
+			// We will see this at logrus.InfoLevel as this is probably going to
+			// indicate that there is something wrong outside of this program.
+			// It could also be argued to call 'panic' here, as this will likely
+			// occur for each attempt of this call.
+			logrus.Errorf("Error creating file %s: %s", target.name, err.Error())
 			continue
 		}
+
+		// Copy the resulting request body to the file that you created.
 		size, err := io.Copy(out, res.Body)
+		// The tick again
 		if err != nil {
-			logrus.Errorf("Error writing file %s: %s", target.Name, err.Error())
+			// We will see this at logrus.InfoLevel as this is probably going to
+			// indicate that there is something wrong outside of this program.
+			// As above, this could be a 'panic' situation and could reasonably
+			// be assumed to occur on each iteration of the loop.
+			logrus.Errorf("Error writing file %s: %s", target.name, err.Error())
 			continue
 		}
-		logrus.Debugf("Wrote %d bytes for file %s", size, target.Name)
+
+		// If we're debugging, let us know the size of the file.
+		logrus.Debugf("Wrote %d bytes for file %s", size, target.name)
+
+		// Close the response body. This can technically return an error, but if
+		// it does, there's nothing that you can really do about it.
 		res.Body.Close()
 	}
 
+	logrus.Debug("The channel of type Target is closed and empty- returning")
 }
 
-// genTargets generates targets and sends to a channel
-func genTargets(targets chan<- Target, config Config) {
-	// < is a less expensive operation than <=,  we will add 1 to the condition
+// genTargets generates targets and sends them to a channel
+func genTargets(targets chan<- target, config Config) {
+
+	// < is a less expensive operation than <=, we will add 1 to the condition.
+	// <= is potentially optimized by the compiler, but this explicitly does
+	// only one comparison.
 	for i := config.Start; i < config.End+1; i += config.Step {
+
+		// We must convert the 'int' to a string to build out our request or we
+		// will receive a type mismatch error.
 		inc := strconv.Itoa(i)
+
+		// This is our file name
 		file := inc + config.Suffix
+
+		// This is the fully qualified path to the file
 		fq := config.URI + file
 		logrus.Debugf("Sending: %s", fq)
-		targets <- Target{URI: fq, Name: file}
+
+		// Send the target to the channel. Take note of the inline declaration
+		// or the struc and the assignment of values.
+		targets <- target{uri: fq, name: file}
 	}
 
 	logrus.Debug("Closing the target channel of type Target")
